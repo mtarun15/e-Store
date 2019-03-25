68b836cda93ee1b6d6fea48820c4114830a62f12
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-03-25 11:51:22 +0530

    addition of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hello World</h1>
-      </div>
+    <React.Fragment>
+      <h3>hello from app</h3>
+    </React.Fragment>
     );
   }
 }
